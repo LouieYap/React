66c014c01104f5e67a7f1421e7f6c4ec718a9e0c
Louie Yap <LouieY@magenic.com>
2018-08-29 14:29:26 +0800

    Week2 Changes

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, TextInput, Text, View, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,22 +20,57 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
+      <View>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Week2/>
       </View>
+
+    );
+  }
+
+
+}
+
+class Week2 extends Component {
+
+ constructor(props) {
+    super(props);
+    this.state = {text: 'Louie', isHidden: true};
+  }
+
+  onButtonPress = () => {
+    this.setState({
+      isHidden: false
+           });
+  }
+
+  render() {
+    return (
+      <View>
+        <TextInput
+          style={{height: 40}}
+          placeholder="TextBox"
+          onChangeText={(text) => this.setState({text})}/>
+            { !this.state.isHidden &&
+            <Text style={{padding: 10, fontSize: 42}}>
+                            {this.state.text}
+            </Text>
+            }
+          <Button onPress={this.onButtonPress} title="Show Text" color="#841584"/>
+      </View>
+
     );
   }
 }
 
+
+
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+
   welcome: {
     fontSize: 20,
     textAlign: 'center',
